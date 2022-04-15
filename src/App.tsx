94cd532f1b30beec601ccb0d313742a355593b19
x22tri <x22tri@gmail.com>
2022-04-15 17:02:55 +0200

    Basic outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+
+import CHARS from './CHARS'
 import './App.css'
 
-function App() {
+import { Search } from 'react-bootstrap-icons'
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const findWord = (word: string): void => {
+    let foundWord = CHARS.find((element) => element.word === word)
+    if (foundWord) {
+      console.log(foundWord)
+    } else {
+      console.log('A szó nem található.')
+    }
+  }
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>test</p>
-      </header>
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault()
+          findWord(searchTerm)
+        }}
+      >
+        <Form.Label>
+          Keress rá egy magyar szóra, hogy megtudd, hogyan fog változni a
+          következő 500 évben!
+        </Form.Label>
+        <InputGroup>
+          <Form.Control
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <Button variant='primary' type='submit' id='search-button'>
+            <Search className='search-icon' />
+          </Button>
+        </InputGroup>
+        <Form.Text className='text-muted'>
+          A szót szótári alakban add meg. (Igéknél ez az E/3 alak.)
+        </Form.Text>
+      </Form>
     </div>
   )
 }
---
+++ src/CHARS.js
@@ -0,0 +1,34 @@
+const CHARS = [
+  {
+    id: 1,
+    word: 'kapcsos',
+    partOfSpeech: 'adjective',
+    declensionType: '-osADJ',
+    use: [
+      {
+        useId: 1,
+        meaning: 'Olyan <tárgy, eszköz>, amelyen kapocs van.',
+        example: 'kapcsos ruha',
+      },
+      {
+        useId: 2,
+        meaning: 'Kapocs alakú.',
+        example: 'kapcsos zárójel',
+      },
+    ],
+    laterDevelopments: [
+      {
+        dateAttested: 2053,
+        use: [
+          {
+            useId: 3,
+            meaning: '<Vmivel> kapcsolatos.',
+            example: 'az örökléssel kapcsos ügyek',
+          },
+        ],
+      },
+    ],
+  },
+]
+
+export default CHARS
