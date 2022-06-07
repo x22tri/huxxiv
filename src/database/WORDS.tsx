848e13347047030dff889aff1edef1dbc0ff80f9
x22tri <x22tri@gmail.com>
2022-06-07 17:46:12 +0200

    Add missing file

---
+++ src/database/WORDS.tsx
@@ -0,0 +1,38 @@
+import { Word } from '../types'
+
+const WORDS: Word[] = [
+  {
+    id: 1,
+    data: [
+      { word: 'kapcsos' },
+      { partOfSpeech: 'melléknév', vowelHarmony: 'o' },
+      {
+        meaning: 'Olyan <tárgy, eszköz>, amelyen kapocs van.',
+        examples: ['kapcsos ruha', 'kapcsos pénztárca'],
+        disappears: [2120, 2230],
+      },
+      {
+        meaning: 'Kapocs alakú.',
+        examples: ['kapcsos zárójel'],
+      },
+      {
+        meaning: '<Vmivel> kapcsolatos, összefüggésben lévő.',
+        examples: ['az örökléssel kapcsos ügyek'],
+        appears: [2045, 2100],
+      },
+    ],
+  },
+  {
+    id: 2,
+    data: [
+      { word: 'szamár' },
+      {
+        partOfSpeech: 'főnév',
+        vowelHarmony: 'o',
+        classes: ['rövidülő', 'nyitótő'],
+      },
+    ],
+  },
+]
+
+export { WORDS }
